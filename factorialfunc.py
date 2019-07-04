dfee58ef55a94e97901cab42afbdd9a89675f566
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-04 09:41:17 +0530

    Add files via upload

---
+++ factorialfunc.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if(n<=20):
+        if(n>0):
+            n= n*factorial(n-1)
+            return n
+        else:
+            return n-1
+    else:
+        return ("give value less tahn 20")
+number=int(input())
+print(factorial(number))
