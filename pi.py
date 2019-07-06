b094dfddbd92f195f78d7a5ad39202a07f3f5e91
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-06 11:22:32 +0530

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,6 +1,6 @@
 input1,input2=map(int,input().split())
 count=0
-for number in range(input1+1,input2):
+for number in range(input1,input2+1):
     if(number > 1):
         for x in range(2,number):
             if((number%x)==0):
