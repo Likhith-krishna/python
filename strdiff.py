a1ab405c34571ee154220abe9b132e597367aec5
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-06 13:25:14 +0530

    Update strdiff.py

--- strdiff.py
+++ strdiff.py
@@ -1,12 +1,14 @@
-a,b=map(str,input().split())
-count=0
-if(len(a)==len(b)):
-    for i in range(0,len(a)):
-        if(a[i]==b[i]):
-            continue
+def strdifference(a,b):
+    count=0
+    if(len(a)==len(b)):
+        for i in range(0,len(a)):
+            if(a[i]==b[i]):
+                continue
+            else:
+                count=count+1
+        if(count==1):
+            return ("yes")
         else:
-            count=count+1
-if(count==1):
-    print("yes")
-else:
-    print("no")
+            return ("no")
+a,b=map(str,input().split())
+print(strdifference(a,b))
