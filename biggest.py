d4e7a76cbc4b90eb2b466cd921bf22294fda9aec
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-06 10:21:24 +0530

    Update biggest.py

--- biggest.py
+++ biggest.py
@@ -1,8 +1,3 @@
-inw=[]
-n=int(input())
-for x in range (0,n):
-    b=int(input())
-    inw.append(b)
-    inw.sort()
-    
-print(inw[-1])
+number=int(input())
+arr=map(int,input().split())
+print(max(arr)))
