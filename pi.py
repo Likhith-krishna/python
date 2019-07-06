e9f23e854673370db4a871c5437130e5308ea1a8
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-06 11:21:02 +0530

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,8 +1,11 @@
 input1,input2=map(int,input().split())
+count=0
 for number in range(input1+1,input2):
     if(number > 1):
         for x in range(2,number):
             if((number%x)==0):
                 break
         else:
-            print(number)
+            count=count+1
+
+print(count)
