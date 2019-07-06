eae93a50cbbaf88e9dd061f2d20a03bdd4077cac
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-06 08:27:15 +0530

    Add files via upload

---
+++ armstronginterval.py
@@ -0,0 +1,13 @@
+inpu1,inpu2=map(int,input().split())
+for inpu in range(inpu1,inpu2+1):
+    suma=0
+    temp=inpu
+    while temp>0:
+        digits=temp%10
+        cube=digits*digits*digits
+        suma=suma+cube
+        temp//=10
+    if(inpu==suma):
+        print(inpu,end=" ")
+
+    
