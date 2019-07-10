9312ee235bc7abdbba8b0d90316e6d5e03c63fe7
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-10 14:10:47 +0530

    Update 3.py

--- hunter/set1/3.py
+++ hunter/set1/3.py
@@ -1,5 +1,12 @@
 iner=int(input())
+flag=0
 a=list(map(int,input().split()))
 for i in range(0,iner):
     if (i==a[i]):
+        flag=1
         print(i,end=" ")
+if(flag==0):
+    print("-1")
+
+        
+  
