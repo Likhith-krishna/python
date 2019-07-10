3e60261ad0eb34d8108ca781b562b94f68f83c3b
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-10 13:07:57 +0530

    Add files via upload

---
+++ player/set2/19.py
@@ -0,0 +1,12 @@
+inpu=int(input())
+for x in range(2,inpu+1):
+    if(inpu%2==0):
+        flag =1
+        for y in range(2,(x//2+1)):
+            if(x%y==0):
+                flag=0
+                break
+        if(flag==1):
+            print(x,end=" ")
+            
+        
