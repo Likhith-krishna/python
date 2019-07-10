09d9f908ab9267e474fe112e6796cdc8f70a1f4d
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-10 09:25:16 +0530

    Add files via upload

---
+++ player/set2/17.py
@@ -0,0 +1,15 @@
+def greaterdiv(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            greater = i
+            
+    return greater
+def lcm(x,y):
+    return (x*y) / greaterdiv(x,y)
+
+number1,number2=map(int,(input().split()))
+print(lcm(number1, number2))
