c2ceabb295af8bb85a37eb92ffba547160865d16
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-14 17:53:28 +0530

    Update 20.py

--- player/set2/20.py
+++ player/set2/20.py
@@ -1,9 +1,13 @@
-stri=str(input())
-arr="0ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-naya=""
-for x in stri:
-    if x in arr:
-        temp=arr.index(x)
-        temp=temp+3
-        naya=naya+arr[temp]
-print(naya)
+inter=list(input())
+finale=[]
+letters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in inter:
+    if i=='X':
+        final.append("A")
+    elif i=='Y':
+        final.append("B")
+    elif i=="Z":
+        final.append("C")
+    else:
+        final.append(letters[letters.index(i)+3])
+print("".join(finale))
