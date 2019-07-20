6240001ae9ce697eaa68f7f066f9221ff1e141b9
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-20 15:46:42 +0530

    Update 98.py

--- beginner/set4/98.py
+++ beginner/set4/98.py
@@ -1,11 +1,7 @@
-'''aw=int(input())
+aw=int(input())
 dd=list(map(int,input().split()))
 for i in range(aw-1):
     if(dd[i]>dd[i+1]):
         print(i)
-        break'''
-sc=int(input())
-ddc=list(map(int,input().split()))
-for i in range(sc-1):
-    if(ddc[i]>ddc[i+1]):
-        print(i)
+        break
+
