3e96cb150d0c88a08ca999050b32d7a1598fb25a
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-21 12:13:54 +0530

    Update h2.py

--- hunter/set1/h2.py
+++ hunter/set1/h2.py
@@ -4,7 +4,7 @@ te=max(intro)
 c1,d1=0,0
 for i in range(0,len(intro)-1):
     for j in range(1,len(intro)):
-        if(intro[i]+intro[j])<te:
+        if abs(intro[i]+intro[j])<te:
             c1,d1=intro[i],intro[j]
             te=abs(c1+d1)
 print(c1,d1)
