b056fa0ab98a93aa1a881654c6d554f2546d2389
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-21 12:12:00 +0530

    Add files via upload

---
+++ hunter/set1/h2.py
@@ -0,0 +1,11 @@
+inter=int(input())
+intro=list(map(int,input().split()))
+te=max(intro)
+c1,d1=0,0
+for i in range(0,len(intro)-1):
+    for j in range(1,len(intro)):
+        if(intro[i]+intro[j])<te:
+            c1,d1=intro[i],intro[j]
+            te=abs(c1+d1)
+print(c1,d1)
+
