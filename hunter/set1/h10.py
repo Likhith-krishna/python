d403044a6f24a5f88799d141c2ed331f8b3c7134
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-23 22:19:07 +0530

    Add files via upload

---
+++ hunter/set1/h10.py
@@ -0,0 +1,11 @@
+e,f=map(int,input().split())
+k=input().split()
+j=input().split()
+for i in k:
+    c=(" ".join(k))
+for l in j:
+    d=(" ".join(j))
+if(c.find(d)==-1):
+    print("no")
+else:
+    print("yes")
