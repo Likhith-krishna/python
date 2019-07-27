180d46b9bf5d14a652e32fae4abb3a71d1957b2b
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-27 07:51:50 +0530

    Update h124.py

--- hunter/set1/h124.py
+++ hunter/set1/h124.py
@@ -1,5 +1,5 @@
-inter=int(input())
-for i in range(0,inter):
-    for j in range(0,inter-i):
+intera=int(input())
+for i in range(0,intera):
+    for j in range(0,intera-i):
         print("1",end=" ")
     print()
