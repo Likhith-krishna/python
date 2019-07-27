c6507f39f54da2f094d97b2d65d4edbd7f464894
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-07-27 07:50:44 +0530

    Update h124.py

--- hunter/set1/h124.py
+++ hunter/set1/h124.py
@@ -1,7 +1,5 @@
 inter=int(input())
-lis=[]
 for i in range(0,inter):
-    for j in range(i,inter):
-        lis.append("1")
-    print(*lis)
-    li=[]
+    for j in range(0,inter-i):
+        print("1",end=" ")
+    print()
