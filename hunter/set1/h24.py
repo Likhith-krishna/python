38e140e42e645ed8ff24b3b3f3fd94ff6c072ab5
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-08-04 14:10:00 +0530

    Update h24.py

--- hunter/set1/h24.py
+++ hunter/set1/h24.py
@@ -3,10 +3,10 @@ k=[int(x) for x in input().split()]
 flag=0
 for i in range(0,v):
     for j in range(i+1,v):
-        b=int(k[i]+k[j])
+        b=k[i]+k[j]
         if(b==n):
             flag=flag+1
-if(flag==1):
+if(flag>=1):
     print("YES")
 else:
     print("NO")
