7478b092c82bc5316bc47e7c665f08fd6ad389dd
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-08-04 15:27:02 +0530

    Update h112.py

--- hunter/set1/h112.py
+++ hunter/set1/h112.py
@@ -1,6 +1,13 @@
 a=input()
 b=input()
+li=[]
+new=[]
 for i in range(0,len(a)):
     if a[i] in b:
-        print(a[i],end="")
+        li.append(a[i])
+for num in li:
+    if num not in new:
+        new.append(num)
+for i in new:
+    print(i,end="")
         
