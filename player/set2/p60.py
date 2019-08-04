033ea9ae9fd5c6c0faa5cb27290bfecff8719ad6
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-08-04 19:12:13 +0530

    Update p60.py

--- player/set2/p60.py
+++ player/set2/p60.py
@@ -1,5 +1,10 @@
-a,b=input().split()
-if a in b:
+m,n=map(str,input().split())
+flag=0
+for i in m:
+    for j in n:
+        if i==j:
+            flag=flag+1
+if(flag>=1):
     print("yes")
 else:
     print("no")
