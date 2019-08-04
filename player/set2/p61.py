9a0858a297d647083809849714cc995e31a23c9c
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-08-04 14:49:48 +0530

    Add files via upload

---
+++ player/set2/p61.py
@@ -0,0 +1,11 @@
+c,d=map(int,input().split())
+l=list(map(int,input().split()))
+flag=0
+for i in range(0,c):
+    for j in range(i+1,c):
+        if l[i]+l[j]==d:
+            flag=flag+1
+if(flag>=1):
+    print("YES")
+else:
+    print("NO")
