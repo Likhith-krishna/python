5f4777a5892a89134bb7d799dd5dd211fc949f85
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-08-05 20:07:46 +0530

    Update p190.py

--- player/set2/p190.py
+++ player/set2/p190.py
@@ -1,11 +1,9 @@
-x,y,z=map(int,input().split())
+x,y,z=int(input().split())
 x=x*x
 y=y*y
 z=z*z
 k=x+y
 if(k==z):
-    print("no")
-elif((x==y) and (y==z)):
-    print("no")
-else:
     print("yes")
+else:
+    print("no")
