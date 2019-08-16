9ee09cbfc01779989c2c53873eeffbc282f70f1e
Likhith-krishna <40300070+Likhith-krishna@users.noreply.github.com>
2019-08-16 20:41:41 +0530

    Add files via upload

--- player/set2/p31.py
+++ player/set2/p31.py
@@ -1,12 +1,6 @@
-op=input()
-count1=0
-count2=0
-for i in op:
-    if i == "(":
-        count1=count1+1
-    else:
-        count2=count2+1
-if(count1==count2):
-    print("yes")
-else:
-    print("no")
+n=int(input())
+a=list(map(int,input().split()))
+result=1
+for i in range(0,n):
+    result=result*a[i]
+print(result)
